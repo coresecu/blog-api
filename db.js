afe8a3ddd4b8002e3e058e821ce15a9fc60cd14f
coresecu <coresecu@proton.me>
2023-08-11 20:27:43 +0900

    Update DB infomation

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-    host: "localhost",
+    host: "svc.sel3.cloudtype.app:30191",
     user: "root",
     password: "AkflAkfldk!@34",
     database: "blog"
