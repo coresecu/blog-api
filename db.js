4ff5363a870cb59bb65e7e99545fba9feb9dc5e0
coresecu <coresecu@proton.me>
2023-08-11 20:41:57 +0900

    Update DB port

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-    host: "svc.sel3.cloudtype.app:30191",
+    host: "svc.sel3.cloudtype.app",
+    port: "30191",
     user: "root",
     password: "AkflAkfldk!@34",
     database: "blog"
